17a9e00541dbc918133fcf34ed390adc9d8d4e08
Caitlyn Ralph <caitlynr@caitlynr-macbookpro.roam.corp.google.com>
2018-10-28 20:02:00 -0400

    pseudocode for first technical prototype

---
+++ graphic.js
@@ -0,0 +1,11 @@
+
+
+// pull the chart and save in an array
+
+// look up the song on genius and pull its lyrics, save in an object
+
+// run sentiment on each song's lyrics and save score in an object
+
+// for now: print each song and color based on sentiment
+
+// later: plot on screen according to ranking + score
